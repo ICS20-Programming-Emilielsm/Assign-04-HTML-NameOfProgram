a2776dfba68e249a864a1666bf508bc741342750
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-05-10 00:20:08 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -11,21 +11,21 @@
  */
 function orderSent() {
   // declare constants
-  const INDIV_PRICE = 10.00
-  const GRP_PRICE = 20.00
-  const PRTY_PRICE = 50.00
-  const ICING_PRICE = 7.00
-  const SPRINKLE_PRICE = 5.00
-  const ICEBORD_PRICE = 15.00
-  const WATR_PRICE = 2.00
-  const ICDTEA_PRICE = 4.00
-  const COFE_PRICE = 4.50
-  const HST = 0.13
-
-  let baseCake = 0
+  const INDIV_PRICE = 10.00;
+  const GRP_PRICE = 20.00;
+  const PRTY_PRICE = 50.00;
+  const ICING_PRICE = 7.00;
+  const SPRINKLE_PRICE = 5.00;
+  const ICEBORD_PRICE = 15.00;
+  const WATR_PRICE = 2.00;
+  const ICDTEA_PRICE = 4.00;
+  const COFE_PRICE = 4.50;
+  const HST = 0.13;
   
   	// initialize variables
-
+  let sizePrice = 0
+  let topPrice = 0
+  let drinkPrice = 0
   	
 	// get age and day of the week	
 	let size = document.getElementById('size');
@@ -33,38 +33,40 @@ function orderSent() {
   let drink = document.getElementById('drink');
 
   //find out price
-  if ((size == "Individual") && (topping == "icing")) {
-		baseCake = INDIV_PRICE + ICING_PRICE
+  if (size == "Individual")  {
+		sizePrice = INDIV_PRICE 
 	}
 	else if (size == "Group") {
 		sizePrice = GRP_PRICE
 	}
-	else if (size == "Party") {
+	else (size == "Party") {
 		sizePrice = PRTY_PRICE
 	}
-   else if (size == "icing") {
+  // toppings
+   if (topping == "icing") {
 		topPrice = ICING_PRICE
 	}
-	else if (size == "sprinkles") {
+	else if (topping == "sprinkles") {
 		topPrice = SPRINKLE_PRICE
 	}
-	else if (size == "icingBorder") {
+	else (topping == "icingBorder") {
 		topPrice = ICEBORD_PRICE
 	}
-  else if (size == "water") {
+  //drinks
+  if (drink == "water") {
 		drinkPrice = WATR_PRICE
 	}
-	else if (size == "icedTea") {
+	else if (drink == "icedTea") {
 		drinkPrice = ICDTEA_PRICE
 	}
-	else if (size == "coffee") {
+	else (drink == "coffee") {
 		drinkPrice = COFE_PRICE
 	}
-    
-	let subtotal = baseCake + drinkPrice + topPrice + sizePrice
+  // Totals
+	let subtotal =  + drinkPrice + topPrice + sizePrice
   let tax = subtotal * HST
   let total = subtotal + tax
     	
-  	// display the greeting
+  	// display the cost
   document.getElementById("display-cost").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
 }
