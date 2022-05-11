f04d2246575475ecef2ebe5fb8092c30c435f59a
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-05-11 00:59:06 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-// Copyright (c) 2022 Ms Raffin All rights reserved
-//
-// Created by: Ms Raffin
-// Created on: March 2022
-// This file contains the JS functions for index.html
 
-"use strict"
+'use strict'
 
 /**
  * This function displays the discount depending on age and day of the week.
  */
-function orderSent() {
+function displayCost() {
+  
   // declare constants
-  const INDIV_PRICE = 10.00;
-  const GRP_PRICE = 20.00;
-  const PRTY_PRICE = 50.00;
-  const ICING_PRICE = 7.00;
+  const INDIV_PRICE = 20.00;
+  const GRP_PRICE = 50.00;
+  const PRTY_PRICE = 100.00;
+  const ICING_PRICE = 10.00;
   const SPRINKLE_PRICE = 5.00;
-  const ICEBORD_PRICE = 15.00;
+  const ICEBORD_PRICE = 25.00;
   const WATR_PRICE = 2.00;
   const ICDTEA_PRICE = 4.00;
-  const COFE_PRICE = 4.50;
+  const COFE_PRICE = 4.00;
   const HST = 0.13;
   
   	// initialize variables
@@ -28,9 +24,9 @@ function orderSent() {
   let drinkPrice = 0
   	
 	// get age and day of the week	
-	let size = document.getElementById('size');
-  let topping = document.getElementById('topping');
-  let drink = document.getElementById('drink');
+	let size = document.getElementById('size').value;
+  let topping = document.getElementById('topping').value;
+  let drink = document.getElementById('drink').value;
 
   //find out price
   if (size == "Individual")  {
@@ -39,9 +35,10 @@ function orderSent() {
 	else if (size == "Group") {
 		sizePrice = GRP_PRICE
 	}
-	else (size == "Party") {
+	else if (size == "Party") {
 		sizePrice = PRTY_PRICE
 	}
+  
   // toppings
    if (topping == "icing") {
 		topPrice = ICING_PRICE
@@ -49,9 +46,10 @@ function orderSent() {
 	else if (topping == "sprinkles") {
 		topPrice = SPRINKLE_PRICE
 	}
-	else (topping == "icingBorder") {
+	else if (topping == "icingBorder") {
 		topPrice = ICEBORD_PRICE
 	}
+  
   //drinks
   if (drink == "water") {
 		drinkPrice = WATR_PRICE
@@ -59,14 +57,15 @@ function orderSent() {
 	else if (drink == "icedTea") {
 		drinkPrice = ICDTEA_PRICE
 	}
-	else (drink == "coffee") {
+	else if (drink == "coffee") {
 		drinkPrice = COFE_PRICE
 	}
+  
   // Totals
-	let subtotal =  + drinkPrice + topPrice + sizePrice
+	let subtotal = drinkPrice + topPrice + sizePrice
   let tax = subtotal * HST
   let total = subtotal + tax
     	
-  	// display the cost
-  document.getElementById("display-cost").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
+  // display the cost
+  document.getElementById("cost").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
 }
